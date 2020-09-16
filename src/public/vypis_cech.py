12cf8768d0943e40b845e39c66915bce31db2ee6
cechjan <46921022+cechjan@users.noreply.github.com>
2020-09-16 19:45:01 +0200

    Update vypis_cech.py

--- src/public/vypis_cech.py
+++ src/public/vypis_cech.py
@@ -4,4 +4,4 @@ from .views import blueprint
 def Vypis_cech():
         pole=[[0,1],[2,3],[0,30]]
         pole[0][1]=pole[0][1]+1
-        return render_template("public/vypis_cech.tmpl", data = pole)
+        return render_template("public/views_cech.tmpl", data = pole)
